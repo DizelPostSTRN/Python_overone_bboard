e4deeb7b06bdac3b9707d846302f71f8285dd4e5
DizelPostSTRN <ragno1@mail.ru>
2021-11-29 19:22:06 +0300

    10

--- samplesite/bboard3/views.py
+++ samplesite/bboard3/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from .models import Вb
+from .models import Bb
 
 def index(request):
     bbs = Bb.objects.order_by('-published')
     return render(request, 'bboard/index.html', {'bbs': bbs})
-
-
-
-
