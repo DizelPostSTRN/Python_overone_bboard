45b98ce85458e4c6d3b3e6eeb619542c69e4740f
DizelPostSTRN <ragno1@mail.ru>
2021-12-01 21:45:14 +0300

    16

--- samplesite/bboard3/urls.py
+++ samplesite/bboard3/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import index, by_rubric
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
 ]
